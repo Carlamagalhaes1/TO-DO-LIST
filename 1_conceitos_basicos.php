215f198884ef29db89f1c965af1ac73ab34951c9
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-08-20 16:50:39 -0300

    Concatenação - união de strings

--- 1_conceitos_basicos.php
+++ 1_conceitos_basicos.php
@@ -6,7 +6,20 @@ echo "teste <br>";
 
 //variaveis
 $nome = "carla";
-echo "Meu nome é $nome"
+echo "Meu nome é $nome <br>";
+
+//Concatenação - união de strings
+
+$sobrenome = "Magalhães";
+$nomeCompleto =$nome . " " . $sobrenome ;
+echo "Meu nome completo é $nomeCompleto <br>";
+
+$irmãos = "Saulo, Davi";
+$pais = ",Carlos e sirlândia";
+
+$familia = $irmãos . " " . $pais;
+echo "Minha familia são $familia"
+
 ?>
 
 
